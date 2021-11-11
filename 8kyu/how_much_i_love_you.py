ee22f0b332845a6d60917fc55cc2582098496bdd
changchiayu525 <68886730+changchiayu525@users.noreply.github.com>
2021-11-11 15:27:34 +0800

    Update how_much_i_love_you.py

--- 8kyu/how_much_i_love_you.py
+++ 8kyu/how_much_i_love_you.py
@@ -25,6 +25,6 @@ def how_much_i_love_you(nb_petals):
     elif nb_petals % 6 == 0:
         return "not at all"
 
-# other user's sollution    
+# other user's solution    
 def how_much_i_love_you(nb_petals):
     return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][nb_petals % 6 - 1]
