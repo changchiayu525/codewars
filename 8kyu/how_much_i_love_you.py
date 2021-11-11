b7fd6a07443206f7745288bd3b08df00bc40735d
changchiayu525 <68886730+changchiayu525@users.noreply.github.com>
2021-11-11 15:24:13 +0800

    Create how_much_i_love_you.py

---
+++ 8kyu/how_much_i_love_you.py
@@ -0,0 +1,26 @@
+"""
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where .nb_petals > 0
+"""
+def how_much_i_love_you(nb_petals):
+    if nb_petals % 6 == 1:
+        return "I love you"
+    elif nb_petals % 6 == 2:
+        return "a little"
+    elif nb_petals % 6 == 3:
+        return "a lot"
+    elif nb_petals % 6 == 4:
+        return "passionately"
+    elif nb_petals % 6 == 5:
+        return "madly"
+    elif nb_petals % 6 == 0:
+        return "not at all"
